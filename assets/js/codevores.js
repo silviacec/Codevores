b49cba29219a92b165873ea50001dd5d4dd10499
silviacec <silviacecuttimsa@gmail.com>
2020-03-26 11:08:11 +0100

    add json actualités

--- assets/js/codevores.js
+++ assets/js/codevores.js
@@ -11,6 +11,25 @@ document.querySelector('.hamburger').addEventListener('click', function() {
 });
 document.querySelector('.aside').style.display='none';
 
-// $('.collapse').collapse()
-// geekzone
-// https://www.geekzone.fr/category/web/feed/json
+// Actualités Geekzone:
+
+function afficheArticle(article) {
+
+  let element = document.createElement("li");
+
+  let lien = document.createElement("a");
+  lien.href = article.url;
+  lien.textContent = article.title;
+
+  element.appendChild(lien);
+  return element;
+}
+
+let liste = document.createElement("ul");
+
+for(let i = 0; i < 10; i++) {
+  article = afficheArticle(data.items[i]);
+  liste.appendChild(article);
+}
+
+document.querySelector(".geekzone").appendChild(liste);
